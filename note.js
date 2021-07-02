a084ce359f3a40074c36ca78ce3c0bc2380fc200
wulebo-username <15700153240@126.com>
2021-07-02 20:25:43 +0800

    submit

---
+++ note.js
@@ -0,0 +1,9 @@
+{
+    /**
+     * sudo cnpm create react-app demo
+     * sudo n stable // 升级到稳定版本
+     * cnpm install antd --save
+     * cnpm install --save @ant-design/icons
+     * cnpm install --save rc-queue-anim
+     */
+}
